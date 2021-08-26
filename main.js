0dc1c7fba6125bbff1d735692a075b03cc726072
sham-sham <shamilah_faria@yahoo.com>
2021-08-25 20:57:59 -0400

    added css file

---
+++ main.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const title = document.createElement('h1');
+    title.innerText = 'Timer';
+    document.querySelector('body').appendChild(title);
+    // make AJAX call here....
+
+
+    var intervalID = setInterval(timer, 500)
+
+   timer(){
+       
+
+
+   }
+  
+
+
+
+
+
+
+
+});
