e1c7dec60752723e1319dad79a3244568fd4ff7c
sham-sham <shamilah_faria@yahoo.com>
2021-08-26 17:35:06 -0400

    added audio files

--- main.js
+++ main.js
@@ -1,11 +1,29 @@
 document.addEventListener('DOMContentLoaded', () => {
  var displayButton = document.querySelector('#timer');
+ displayButton.setAttribute("height", "30px");
  var pauseButton = document.querySelector('#pause');
  var resetButton = document.querySelector('#reset');
  let input = '00:00:00';
  let totalSeconds = (parseInt(input[0], 10) * 3600) + (parseInt(input[1] ,10) * 60) + parseInt(input[2],10);
  console.log(totalSeconds);
-
+    var oneHourAudio = new Audio('1 hour.mp3');
+    var tenMinAudio = new Audio('10 minutes.mp3');
+    var fiveMinAudio = new Audio('5 minutes.mp3'); 
+    var fourMinAudio = new Audio('4 minutes.mp3');
+    var threeMinAudio = new Audio('3 minutes.mp3');
+    var twoMinAudio = new Audio('2 minutes.mp3');
+    var oneMinAudio = new Audio('1 minutes.mp3');
+    var tenSecAudio = new Audio('10 seconds.mp3');
+    var nineSecAudio = new Audio('9 seconds.mp3');
+    var eightSecAudio = new Audio('8 seconds.mp3');
+    var sevenSecAudio = new Audio('7 seconds.mp3');
+    var sixSecAudio = new Audio('6 seconds.mp3');
+    var fiveSecAudio = new Audio('5 seconds.mp3');
+    var fourSecAudio = new Audio('4 seconds.mp3');
+    var threeSecAudio = new Audio('3 seconds.mp3');
+    var twoSecAudio = new Audio('2 seconds.mp3');
+    var oneSecAudio = new Audio('1 seconds.mp3');
+    var timeUpAudio = new Audio('Time is Up.mp3');
 
 
 
@@ -20,7 +38,7 @@ pauseButton.addEventListener("click", () => clearInterval);
 
 resetButton.addEventListener("click", () => (location.reload()));
 
-function inputReassignment() { 
+function inputReassignment() {
 
     // reassign the hour, minute, and second to input  
     let passed = false;
@@ -47,13 +65,33 @@ let seconds;
 
 
 setInterval(function () {
-
+    
     hours = Math.floor(totalSeconds/3600);
     console.log(hours);
     minutes = Math.floor((totalSeconds - (hours * 3600)) / 60);
     console.log(minutes);
     seconds = totalSeconds - (hours * 3600) - (minutes * 60);
     console.log(seconds);
+
+    //checking time to play audio
+    if(hours === 1 && minutes === 0 && seconds === 0) oneHourAudio.play();
+    if(minutes === 10 && hours === 0 && seconds === 0) tenMinAudio.play();
+    if(minutes === 5 && hours === 0 && seconds === 0) fiveMinAudio.play();
+    if(minutes === 4 && hours === 0 && seconds === 0) fourMinAudio.play();
+    if(minutes === 3 && hours === 0 && seconds === 0) threeMinAudio.play();
+    if(minutes === 2 && hours === 0 && seconds === 0) twoMinAudio.play();
+    if(minutes === 1 && hours === 0 && seconds === 0) oneMinAudio.play();
+    if(seconds === 10 && minutes === 0 && hours === 0) tenSecAudio.play();
+    if(seconds === 9 && minutes === 0 && hours === 0) nineSecAudio.play();
+    if(seconds === 8 && minutes === 0 && hours === 0) eightSecAudio.play();
+    if(seconds === 7 && minutes === 0 && hours === 0) sevenSecAudio.play();
+    if(seconds === 6 && minutes === 0 && hours === 0) sixSecAudio.play();
+    if(seconds === 5 && minutes === 0 && hours === 0) fiveSecAudio.play();
+    if(seconds === 4 && minutes === 0 && hours === 0) fourSecAudio.play();
+    if(seconds === 3 && minutes === 0 && hours === 0) threeSecAudio.play();
+    if(seconds === 2 && minutes === 0 && hours === 0) twoSecAudio.play();
+    if(seconds === 1 && minutes === 0 && hours === 0) oneSecAudio.play();
+    if(seconds === 0 && minutes === 0 && hours === 0) timeUpAudio.play();
 
     // reformatting to hh:mm:ss display
     hours = hours < 10 ? "0" + hours : hours;
@@ -68,6 +106,7 @@ setInterval(function () {
     // decrements total seconds &
     // checks if timer is done
     if (--totalSeconds < 0) {
+        // timeUpAudio.play();
         alert("Timer's up!");
         location.reload();
     }
